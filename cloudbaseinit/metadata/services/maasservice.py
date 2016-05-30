f08bc4cd551725eaf57c1b91a9dfa1638c1fd0bd
Adrian Vladu <avladu@cloudbasesolutions.com>
2016-05-30 20:32:38 +0300

    Add support for MAAS json network metadata
    Implements: blueprint json-network-config
    Co-Authored-By: Paula Madalina Crismaru <pcrismaru@cloudbasesolutions.com>
    
    Depends-On: I6b59c8952ad8f6d920a15711fe6d2d86698db636
    Depends-On: I78eedb1e0f3a6deaad93e27b2a5cdcb77fff07fb
    
    Change-Id: Id46e9cda145fa7d9c6507ae1508589e37e0a0f78

--- cloudbaseinit/metadata/services/maasservice.py
+++ cloudbaseinit/metadata/services/maasservice.py
@@ -12,6 +12,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import copy
+import json
+import netaddr
 import re
 
 from oauthlib import oauth1
@@ -108,3 +111,100 @@ class MaaSHttpService(base.BaseHTTPMetadataService):
 
     def get_user_data(self):
         return self._get_cache_data('%s/user-data' % self._metadata_version)
+
+    def _get_network_data(self):
+        data = self._get_cache_data('%s/network_data.json' %
+                                    self._metadata_version,
+                                    decode=True)
+        if data:
+            return json.loads(data)
+
+    def get_network_details(self):
+        network_data = self._get_network_data()
+        if network_data:
+            return self._parse_network_data(network_data)
+
+    def _parse_network_data(self, network_data):
+        if not network_data:
+            return None
+        network_l2_config = self._parse_l2_network_data(network_data)
+        network_l3_config = self._parse_l3_network_data(network_data,
+                                                        network_l2_config)
+        network_l4_config = self._parse_l4_network_data(network_data)
+        return base.AdvancedNetworkDetails(network_l2_config,
+                                           network_l3_config,
+                                           network_l4_config)
+
+    def _parse_l2_network_data(self, network_data):
+        if not (network_data and network_data.get('config')):
+            return None
+        parsed_links = []
+        for link in network_data['config']:
+            if link.get('type') in ['nameserver']:
+                continue
+            parsed_link = copy.deepcopy(base.L2NetworkDetails)
+            parsed_link['name'] = link.get('id')
+            parsed_link['mtu'] = link.get('mtu')
+            if link.get('mac_address'):
+                parsed_link['mac_address'] = link['mac_address'].upper()
+            if link.get('type'):
+                parsed_link['meta_type'] = link['type']
+                parsed_link['type'] = 'phy'
+                if link['type'] in ['ovs', 'vif']:
+                    parsed_link['type'] = 'phy'
+                elif link['type'] in ['vlan']:
+                    parsed_link['type'] = link['type']
+                    parsed_link['vlan_info']['vlan_id'] = link.get('vlan_id')
+                elif link['type'] == 'bond':
+                    parsed_link['type'] = link['type']
+                    parsed_link['bond_info']['bond_members'] = \
+                        link.get('bond_interfaces')
+                    if link.get('params') and link['params'].get('bond-mode'):
+                        parsed_link['bond_info']['bond_mode'] = \
+                            link['params']['bond-mode']
+            parsed_links.append(parsed_link)
+        return parsed_links
+
+    def _parse_l3_network_data(self, network_data, network_l2_config):
+        if not (network_data and network_data.get('config')):
+            return None
+        parsed_networks = []
+        for network_config in network_data['config']:
+            if not network_config.get('subnets'):
+                continue
+            for subnet in network_config.get('subnets'):
+                parsed_network = copy.deepcopy(base.L3NetworkDetails)
+                parsed_network["id"] = network_config.get('name')
+                parsed_network["name"] = network_config.get('name')
+                parsed_network['gateway'] = subnet.get('gateway')
+                if network_config.get('mac_address'):
+                    parsed_network['mac_address'] = \
+                        network_config['mac_address'].upper()
+                if subnet.get('address'):
+                    parsed_network['prefix'] = subnet['address'].split('/')[1]
+                    parsed_network['netmask'] = (str(netaddr.IPNetwork(subnet
+                                                 ['address']).netmask))
+                    parsed_network['ip_address'] = (subnet['address']
+                                                    .split('/')[0])
+                    version = (netaddr.IPAddress(parsed_network['ip_address'])
+                               .version)
+                    if version == 4:
+                        parsed_network['type'] = 'ipv4'
+                    elif version == 6:
+                        parsed_network['type'] = 'ipv6'
+                parsed_network['meta_type'] = parsed_network['type']
+                if subnet.get('dns_nameservers'):
+                    parsed_network['dns_nameservers'].extend(
+                        subnet['dns_nameservers'])
+                parsed_networks.append(parsed_network)
+        return parsed_networks
+
+    def _parse_l4_network_data(self, network_data):
+        if not (network_data and network_data.get('config')):
+            return None
+        l4_config = copy.deepcopy(base.L4NetworkDetails)
+        for service in network_data['config']:
+            if service.get('type') == 'nameserver':
+                l4_config['global_dns_nameservers'] = service['address']
+                break
+        return l4_config
