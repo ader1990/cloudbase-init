27eb38568739ac8ab7896059a4bc7b30e09022f0
Paula Madalina Crismaru <pcrismaru@cloudbasesolutions.com>
2017-06-26 11:46:38 +0300

    Add FQDN cloud-config plugin
    Change-Id: I020f4f5918a8d2373715406a25b005310e0a70c5

--- cloudbaseinit/plugins/common/userdataplugins/cloudconfigplugins/factory.py
+++ cloudbaseinit/plugins/common/userdataplugins/cloudconfigplugins/factory.py
@@ -24,6 +24,8 @@ PLUGINS = {
                     'cloudconfigplugins.set_timezone.SetTimezonePlugin',
     'set_hostname': 'cloudbaseinit.plugins.common.userdataplugins.'
                     'cloudconfigplugins.set_hostname.SetHostnamePlugin',
+    'set_fqdn': 'cloudbaseinit.plugins.common.userdataplugins.'
+                'cloudconfigplugins.set_fqdn.SetFQDNPlugin',
 }
 
 
