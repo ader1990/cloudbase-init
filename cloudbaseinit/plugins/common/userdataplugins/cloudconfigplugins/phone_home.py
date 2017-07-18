a0596648341baf4bd95da79cdf97aebe8d800ba2
Adrian Vladu <avladu@cloudbasesolutions.com>
2017-07-18 13:29:01 +0300

    Make a GET instead of POST when the phone home data is empty
    Change-Id: I1e19589ba717db5073231695c264cb6d13a5ed69

--- cloudbaseinit/plugins/common/userdataplugins/cloudconfigplugins/phone_home.py
+++ cloudbaseinit/plugins/common/userdataplugins/cloudconfigplugins/phone_home.py
@@ -17,13 +17,14 @@ import json
 from oslo_log import log as oslo_logging
 from six.moves.urllib import error
 
+from cloudbaseinit import conf as cloudbaseinit_conf
 from cloudbaseinit.osutils import factory
 from cloudbaseinit.plugins.common.userdataplugins.cloudconfigplugins import (
     base
 )
-from cloudbaseinit.utils import hostname
 
 
+CONF = cloudbaseinit_conf.CONF
 LOG = oslo_logging.getLogger(__name__)
 
 PHONE_HOME_POST_LIST_ALL = [
@@ -31,6 +32,7 @@ PHONE_HOME_POST_LIST_ALL = [
     'hostname',
     'fqdn',
 ]
+
 
 class PhoneHomePlugin(base.BaseCloudConfigPlugin):
     """Call a specific URL with a specific POST payload.
@@ -68,17 +70,21 @@ class PhoneHomePlugin(base.BaseCloudConfigPlugin):
 
         if not phone_home_retries:
             phone_home_retries = CONF.default.phone_home_retries
-        
+
         if phone_home_post and phone_home_post == 'all':
                 phone_home_post_data_entries = PHONE_HOME_POST_LIST_ALL
         if phone_home_post_data_entries:
             for key in phone_home_post_data_entries:
                 phone_home_post_body[key] = phone_home_available_data[key]
- 
-        LOG.info("Calling home to url %s with data %r", phone_home_url, phone_home_post_body)
+
+        phone_home_data = None
+        if phone_home_post_body:
+            phone_home_data = json.dumps(phone_home_post_body)
+        LOG.info("Calling home to url %s with data %r", phone_home_url,
+                 phone_home_data)
         try:
             action = lambda: service._http_request(
-                phone_home_url, json.dumps(phone_home_post_body))
+                phone_home_url, phone_home_data)
             return service._exec_with_retry(action)
         except error.HTTPError:
             LOG.error("Failed to call home to the metadata service")
